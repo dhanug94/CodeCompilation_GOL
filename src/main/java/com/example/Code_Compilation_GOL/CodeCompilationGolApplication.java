c56277afe71375dbb621fb73e7f4d28cac4461b2
unknown <dhanashri.galgalikar11@gmail.com>
2019-05-05 00:19:18 +0530

    Hello

---
+++ src/main/java/com/example/Code_Compilation_GOL/CodeCompilationGolApplication.java
@@ -0,0 +1,13 @@
+package com.example.Code_Compilation_GOL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeCompilationGolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeCompilationGolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Code_Compilation_GOL/CodeCompilationGolApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.Code_Compilation_GOL;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CodeCompilationGolApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
